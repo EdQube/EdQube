fdaf56fd6218f6ffc721679f48ed6f94039c999c
Asus <shivanjalsharma00@gmail.com>
2025-06-15 12:10:30 +0530

    Z

---
+++ app/src/main/java/com/module/edqube/initialScreens/ProfileDetailsFirstActivity.kt
@@ -0,0 +1,16 @@
+package com.module.edqube.initialScreens
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.module.edqube.R
+
+class ProfileDetailsFirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_profile_details_first)
+    }
+}
